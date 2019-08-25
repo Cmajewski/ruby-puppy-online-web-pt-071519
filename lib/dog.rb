95a79a2bc406e19a4d7d1d1d7aa5afdb342940be
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-25 20:07:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ class Dog
 attr_reader :name
 @@all=[]
 
-  def save
-    @@all<<self
-  end
-
   def initialize (name)
     @name=name
   save
+  end
+  
+  def save
+    @@all<<self
   end
 
   def self.all
@@ -17,8 +17,9 @@ attr_reader :name
 
   def self.print_all
     @@all.each do |dog|
-      puts dog
+    string="#{dog}"
     end
+    string
   end
 
   def self.clear_all
